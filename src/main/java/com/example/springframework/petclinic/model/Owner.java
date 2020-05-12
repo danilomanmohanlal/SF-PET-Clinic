dbcbd029f39888a65d9ee411322ae5d867b9835d
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2020-05-12 15:40:25 +0100

    Build model for pet clinic

---
+++ src/main/java/com/example/springframework/petclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package com.example.springframework.petclinic.model;
+
+public class Owner extends Person {
+
+
+}
---
+++ src/main/java/com/example/springframework/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.example.springframework.petclinic.model;
+
+public class Vet extends Person {
+
+}
