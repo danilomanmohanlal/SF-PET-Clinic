909a32aca5adf19269bf2a49805be6080e951954
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2020-05-20 13:15:04 +0100

    Refactor service operations

--- pet-clinic-data/src/main/java/com/example/springframework/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/springframework/services/PetService.java
@@ -2,13 +2,7 @@ package com.example.springframework.services;
 
 import com.example.springframework.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
