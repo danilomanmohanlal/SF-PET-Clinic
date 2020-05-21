f6db8212764fca774f3c63b6f5ca11b086efcf4b
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2020-05-21 23:17:43 +0100

    updated owner and added visit

--- pet-clinic-data/src/main/java/com/example/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/springframework/model/Owner.java
@@ -1,6 +1,17 @@
 package com.example.springframework.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets = new HashSet<>();
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
