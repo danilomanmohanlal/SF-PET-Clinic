3edbf0b7b181590e47603e4b600d7dede082f892
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2020-05-22 00:15:52 +0100

    updated vet and added specialty

---
+++ pet-clinic-data/src/main/java/com/example/springframework/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.example.springframework.model;
+
+public class Specialty extends BaseEntity {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/example/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/springframework/model/Vet.java
@@ -1,5 +1,16 @@
 package com.example.springframework.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Specialty> specialties;
 }
