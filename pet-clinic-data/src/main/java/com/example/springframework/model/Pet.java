597cc04d25ffa33a08ea75dad4c544e2ed0af12f
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2020-05-31 16:44:40 +0100

    updated visit with jpa entity mappings

--- pet-clinic-data/src/main/java/com/example/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/springframework/model/Pet.java
@@ -2,6 +2,8 @@ package com.example.springframework.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,19 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
+
 
     public String getName() {
         return name;
